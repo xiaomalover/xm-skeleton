d2083f4404ff9a693740bedd3c860c52d2539f08
xiaomalover <xiaomalover@gmail.com>
2018-12-27 16:28:05 +0800

    update

--- order-server/src/main/java/com/xm/pay/ServerService.java
+++ order-server/src/main/java/com/xm/pay/ServerService.java
@@ -1,14 +1,11 @@
 package com.xm.pay;
 
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServerService {
 
-
-    public  String  HelloServer1(String str){
-
-        return "这是Server1的本地服务接口,"+str;
+    public String HelloServer1(String str) {
+        return "这是Order Server的本地服务接口," + str;
     }
 }
--- pay-server/src/main/java/com/xm/pay/ServerService.java
+++ pay-server/src/main/java/com/xm/pay/ServerService.java
@@ -1,14 +1,11 @@
 package com.xm.pay;
 
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServerService {
 
-
     public  String  HelloServer2(String str){
-
-        return "这是Server2的本地服务接口,"+str;
+        return "这是Pay Server的本地服务接口,"+str;
     }
 }
