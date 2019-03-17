4cfed360347499a0056e4ab01180e401355f8a37
xiaomalover <xiaomalover@gmail.com>
2019-03-17 14:05:07 +0800

    update

--- user-server/src/main/java/com/xm/user/authorization/resolvers/CurrentUserMethodArgumentResolver.java
+++ user-server/src/main/java/com/xm/user/authorization/resolvers/CurrentUserMethodArgumentResolver.java
@@ -5,13 +5,19 @@ import com.xm.user.authorization.manager.TokenManager;
 import com.xm.common.constant.TokenConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-import org.springframework.web.multipart.support.MissingServletRequestPartException;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 /**
  * 增加方法注入，将含有CurrentUser注解的方法参数注入当前登录用户
@@ -39,7 +45,7 @@ public class CurrentUserMethodArgumentResolver implements HandlerMethodArgumentR
         ModelAndViewContainer mavContainer,
         NativeWebRequest webRequest,
         WebDataBinderFactory binderFactory
-    ) throws Exception {
+    ) throws HttpServerErrorException {
         String token = webRequest.getHeader(TokenConstant.TOKEN_PARAM_NAME);
         if (!ObjectUtils.isEmpty(token)) {
             //续期token
@@ -49,7 +55,6 @@ public class CurrentUserMethodArgumentResolver implements HandlerMethodArgumentR
                 return userId;
             }
         }
-
-        throw new MissingServletRequestPartException(TokenConstant.TOKEN_PARAM_NAME);
+        throw new HttpServerErrorException(HttpStatus.UNAUTHORIZED);
     }
 }
