c772ea2082727de8a517a260b2863961d84a84bd
xiaomalover <xiaomalover@gmail.com>
2019-04-09 13:57:50 +0800

    清除编译警告

--- admin-server/src/main/java/com/xm/admin/config/security/jwt/AuthenticationSuccessHandler.java
+++ admin-server/src/main/java/com/xm/admin/config/security/jwt/AuthenticationSuccessHandler.java
@@ -4,12 +4,10 @@ import cn.hutool.core.util.StrUtil;
 import com.google.gson.Gson;
 import com.xm.admin.common.annotation.SystemLog;
 import com.xm.admin.common.constant.SecurityConstant;
-import com.xm.admin.common.utils.IpInfoUtil;
 import com.xm.admin.common.utils.ResponseUtil;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -38,9 +36,6 @@ public class AuthenticationSuccessHandler extends SavedRequestAwareAuthenticatio
 
     @Value("${skeleton.saveLoginTime}")
     private Integer saveLoginTime;
-
-    @Autowired
-    private IpInfoUtil ipInfoUtil;
 
     @Override
     @SystemLog(description = "登录系统")
