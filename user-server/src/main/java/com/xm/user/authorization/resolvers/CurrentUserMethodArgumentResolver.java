bb8daf05d9e2ff7c88097cc8a04e9a894b6b97aa
xiaomalover <xiaomalover@gmail.com>
2019-04-12 10:47:00 +0800

    删除多余类

--- user-server/src/main/java/com/xm/user/authorization/resolvers/CurrentUserMethodArgumentResolver.java
+++ user-server/src/main/java/com/xm/user/authorization/resolvers/CurrentUserMethodArgumentResolver.java
@@ -11,13 +11,8 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.Objects;
 
 /**
  * 增加方法注入，将含有CurrentUser注解的方法参数注入当前登录用户
--- user-server/src/main/java/com/xm/user/exception/RestErrorFilter.java
+++ user-server/src/main/java/com/xm/user/exception/RestErrorFilter.java
@@ -5,7 +5,6 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletResponse;
 
 /**
